a18df8ab4c14187b2bd66dd698ff3a42c0a12c2f
Devi <167269429+dragydevi@users.noreply.github.com>
2026-08-28 03:19:31 -0400

    Prevent Blood Bound from Converting Traitors and Other Team Antagonists (#1437)
    * added tot blacklist
    
    * fixed

--- Content.Server/_Harmony/GameTicking/Rules/BloodBrotherRuleSystem.cs
+++ Content.Server/_Harmony/GameTicking/Rules/BloodBrotherRuleSystem.cs
@@ -18,13 +18,16 @@ using Content.Shared._Harmony.Roles.Components;
 using Content.Shared.Database;
 using Content.Shared.Humanoid;
 using Content.Shared.IdentityManagement;
+using Content.Shared.Mind;
 using Content.Shared.Mindshield.Components;
 using Content.Shared.Mobs.Systems;
 using Content.Shared.NPC.Systems;
 using Content.Shared.Popups;
 using Content.Shared.Roles.Components;
+using Content.Shared.Roles.RoleCodeword;
 using Content.Shared.Zombies;
 using Robust.Server.Player;
+using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
 
 namespace Content.Server._Harmony.GameTicking.Rules;
@@ -45,6 +48,9 @@ public sealed partial class BloodBrotherRuleSystem : GameRuleSystem<BloodBrother
     [Dependency] private RoleSystem _roleSystem = default!;
     [Dependency] private StunSystem _stunSystem = default!;
     [Dependency] private TargetObjectiveSystem _targetObjectiveSystem = default!;
+
+    private static readonly ProtoId<RoleTypePrototype>[] Filter =
+        {"SoloAntagonist", "TeamAntagonist"}; // Traitors and Nukies and other team antags like... bingles?
 
     public override void Initialize()
     {
@@ -268,6 +274,13 @@ public sealed partial class BloodBrotherRuleSystem : GameRuleSystem<BloodBrother
         if (targetMind.UserId == null)
         {
             errorMessage = "blood-brother-convert-failed-no-mind";
+            return false;
+        }
+
+        //Prevent Traitors - they have RoleCodewordComponent
+        if (Filter.Contains(targetMind.RoleType))
+        {
+            errorMessage = "blood-brother-convert-failed-preference"; // Dont want the BB to metagame a tot with a different message
             return false;
         }
 
