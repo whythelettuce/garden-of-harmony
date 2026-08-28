d162717d9cba1f9d1b91efba56229b48dc2eeda6
Dino <170338647+Dinoguy12349@users.noreply.github.com>
2026-08-28 08:47:26 +0300

    swaps the placement of two code blocks in sharedpnuematiccannonsystem so the bungo bomber cant shoot fucking anti material rounds anymore (#1468)

--- Content.Shared/PneumaticCannon/SharedPneumaticCannonSystem.cs
+++ Content.Shared/PneumaticCannon/SharedPneumaticCannonSystem.cs
@@ -20,12 +20,15 @@ public abstract partial class SharedPneumaticCannonSystem : EntitySystem
 
     private void OnAttemptShoot(EntityUid uid, PneumaticCannonComponent component, ref AttemptShootEvent args)
     {
-        // if the cannon doesn't need gas then it will always predict firing
-        if (component.GasUsage == 0f)
-            return;
+// Harmony Change start - swaps the positioning so the weapons that use 0 gas wont shoot bullets
 
         // pneumatic cannon usually doesn't shoot bullets
         args.ThrowItems = component.ThrowItems;
+
+        // if the cannon doesn't need gas then it will always predict firing
+        if (component.GasUsage == 0f)
+            return;
+// Harmony Change stop
 
         // we don't have atmos on shared, so just predict by the existence of a slot item
         // server will handle auto ejecting/not adding the slot item if it doesnt have enough gas,
